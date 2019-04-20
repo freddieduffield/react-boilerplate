00bc71996f3943cb7059f4f3cdcf3ed63f21645b
FrederickFoxxx <fwhduffield@gmail.com>
2019-04-20 16:10:43 +0100

    added propsed javascript features plugin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    count: 0
+  };
   render() {
-    return <h1>Hello world</h1>;
+    return (
+      <>
+        <h1>Hello world</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+      </>
+    );
   }
 }
 
